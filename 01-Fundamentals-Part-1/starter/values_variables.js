22f661eae4f316d315e98a7cb0827701228484e4
scarter31191 <scarter31191@gmaile.com>
2021-11-25 23:37:35 -0500

    JS training

--- 01-Fundamentals-Part-1/starter/values_variables.js
+++ 01-Fundamentals-Part-1/starter/values_variables.js
@@ -1,7 +1,44 @@
 COUNTRY = "USA"
 CONTINENT = "North America"
-POPULATION = 500,000,000
+let population = 500000000
+ISISLAND = false
+let langue = 'English'
+let finlandPop = 6000000
+let averagePop = 33000000
+const description = 'Portugal is in Europe, and its 11 million people speak portuguese'
 
-console.log(COUNTRY)
-console.log(CONTINENT)
-console.log(POPULATION)
+// console.log(COUNTRY)
+// console.log(CONTINENT)
+// console.log(population)
+// console.log(ISISLAND)
+// console.log(langue)
+
+// If your country split in half, and each half would contain
+// half the population, then how many people would live in 
+// each half?
+console.log(population / 2)
+
+// Increase the population of your country by 1 and log the 
+// result to the console
+
+population++;
+console.log(population)
+
+// Finland has a population of 6 million. Does your country 
+// have more people than Finland?
+
+console.log(population > finlandPop)
+
+// The average population of a country is 33 million people. 
+// Does your country have less people than the average 
+// country?
+
+console.log(population < averagePop)
+
+// Based on the variables you created, create a new variable 
+// 'description' which contains a string with this format: 
+// 'Portugal is in Europe, and its 11 million people speak 
+// portuguese
+
+console.log(description)
+
